230e00bf61dacd970dcb6bbf249bc09c5ebc47e5
pauloojr <paulojr7704@outlook.com>
2026-06-16 18:30:50 -0300

    add iac vaultwarden terraform

---
+++ bucket/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  
+    default_tags {
+      tags = {
+        owner = "Matheus Varela"
+        managed-by = "terraform"
+      }
+    }
+}
+
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+      }
+    }
+} 
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket-ownership" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket-acl" {
+  depends_on = [ aws_s3_bucket_ownership_controls.bucket-ownership ]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl = "private"
+}
+
+resource "aws_s3_bucket_versioning" "bucket-versioning" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
